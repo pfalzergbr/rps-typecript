57e38979531450793b37327c4c036ab8e7834332
Gabor Pfalzer <pfalzer@gmail.com>
2021-03-07 16:07:49 +0000

    add active class to buttons

--- src/components/Choice.tsx
+++ src/components/Choice.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
-// import { capitalize } from '../utils/capitalize';
+import { capitalize } from '../utils/capitalize';
 import Option from './Option';
-import styles from './styles/Choice.module.scss'
+import styles from './styles/Choice.module.scss';
 
 export interface ChoiceProps {
   rules: GameRuleset<ValidRules>;
@@ -24,11 +24,6 @@ const Choice: React.FC<ChoiceProps> = ({ rules, playGame }) => {
 
   return (
     <div className={styles.Choice}>
-      {/* <h2>
-        {choice
-          ? `You are choosing ${capitalize(choice.choiceName)}`
-          : 'Choose your weapon!'}
-      </h2> */}
       <div className={styles.choices}>
         {choices.map((option, index) => (
           <Option
@@ -39,7 +34,14 @@ const Choice: React.FC<ChoiceProps> = ({ rules, playGame }) => {
           />
         ))}
       </div>
-      <button className={styles.playButton} onClick={handlePlayGame}>Play!</button>
+      <p>
+        {choice
+          ? `You are choosing ${capitalize(choice.choiceName)}!`
+          : 'Choose your weapon!'}
+      </p>
+      <button className={styles.playButton} onClick={handlePlayGame}>
+        Play!
+      </button>
     </div>
   );
 };
